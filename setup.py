fe61d98256e51dfa7327cfec809ec40576cb2b7d
Arno Annys <94530834+arnoannys@users.noreply.github.com>
2026-01-22 15:10:45 +0100

    bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
    name='pyEELSMODEL',
-   version='1.0.5',
+   version='1.0.6',
    author='Jo Verbeeck and Daen Jannis',
    author_email='jo.verbeeck@uantwerpen.be',
    package_dir={'': '.'},
